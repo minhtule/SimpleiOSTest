c5197d56659cfa035ddfe337aadcc906053941b3
Minh Tu Le <minhtule05@gmail.com>
2015-03-04 11:08:40 +0800

    Nothing commit

--- SimpleTestTests/SimpleTestTests.swift
+++ SimpleTestTests/SimpleTestTests.swift
@@ -16,6 +16,7 @@ class SimpleTestTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
