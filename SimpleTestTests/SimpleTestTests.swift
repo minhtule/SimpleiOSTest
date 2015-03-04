cd33ae900f12661bbc6d47d0806a1ea22387fbff
Minh Tu Le <minhtule05@gmail.com>
2015-03-04 10:52:41 +0800

    Init simple tests for localhost connection

--- SimpleTestTests/SimpleTestTests.swift
+++ SimpleTestTests/SimpleTestTests.swift
@@ -15,22 +15,41 @@ class SimpleTestTests: XCTestCase {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    func testSuccess() {
+        
+        let expectation = expectationWithDescription("success event")
+        
+        request(.GET, "http://localhost:8000/success")
+            .responseJSON { (request, response, data, error) -> Void in
+                expectation.fulfill()
+                
+                XCTAssertEqual(response!.statusCode, 200, "Status code should be 200");
+            }
+        
+        waitForExpectationsWithTimeout(2) { (error) in
+            XCTAssertNil(error, "\(error)")
         }
     }
     
+    func testError() {
+        
+        let expectation = expectationWithDescription("error event")
+        
+        request(.GET, "http://localhost:8000/error")
+            .responseJSON { (request, response, data, error) -> Void in
+                expectation.fulfill()
+                
+                XCTAssertEqual(response!.statusCode, 404, "Status code should be 404");
+        }
+        
+        waitForExpectationsWithTimeout(2) { (error) in
+            XCTAssertNil(error, "\(error)")
+        }
+    }
 }
